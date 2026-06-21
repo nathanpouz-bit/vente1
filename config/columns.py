d91be25001291c9b79aacba4ea331c827bd24737
nathanpouz-bit <nathanael.pouzin@gmail.com>
2026-06-21 12:10:18 +0200

    Add REQUIRED_COLUMNS list to columns.py

---
+++ config/columns.py
@@ -0,0 +1,18 @@
+REQUIRED_COLUMNS = [
+    "Segment",
+    "Country",
+    "Product",
+    "Discount Band",
+    "Units Sold",
+    "Manufacturing Price",
+    "Sale Price",
+    "Gross Sales",
+    "Discounts",
+    "Sales",
+    "COGS",
+    "Profit",
+    "Date",
+    "Month Number",
+    "Month Name",
+    "Year"
+]
